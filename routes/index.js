7897df5a5ce1f4976d9db6ef8d96c64af6ce3899
Jorge Herrera <jorge.herrera.cabrera@gmail.com>
2014-10-14 22:59:06 -0500

    Sending cookie when user chooses "remember me" option

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var express = require('express'),
+    moment = require('moment'),
     router = express.Router(),
     controllers = require(process.cwd() + '/controllers');
 
@@ -12,19 +13,47 @@ function isLoggedIn(req, res, next) {
     res.redirect('/login');
 }
 
+function issueToken(user, done) {
+    var token = new Buffer(user.email).toString('base64');
+    return done(null, token);
+}
+
 module.exports = function(passport) {
 
-    router.get('/', controllers.getIndex);
+    router.get('/', isLoggedIn, controllers.showNewStaffingRequest);
 
     router.get('/login', controllers.showLogin);
 
     router.post('/login',
         passport.authenticate('local', {
-                successRedirect: '/staffing-request',
                 failureRedirect: '/login',
                 failureFlash: true
             }
-        )
+        ),
+        function(req, res, next) {
+            // Issue a remember me cookie only if the option was checked
+            if (!req.body.remember_me) {
+                return next();
+            }
+
+            issueToken(req.user, function(err, token) {
+                if (err) {
+                    return next(err);
+                }
+
+                var cookieDuration = moment.duration(2, 'minutes').asMilliseconds();
+                res.cookie('staffing_request_remember_me', token, {
+                        path: '/',
+                        httpOnly: true,
+                        maxAge: cookieDuration
+                    }
+                );
+                return next();
+            });
+        },
+        function(req, res) {
+            res.redirect('/staffing-request');
+        }
     );
 
     router.get('/staffing-request', isLoggedIn, controllers.showNewStaffingRequest);
