efc895285326c36d52a3dfdf3c6946a6c138b044
Jorge Herrera <jorge.herrera.cabrera@gmail.com>
2014-12-08 20:48:36 -0600

    Finished basic logic to show position length controls

--- app/js/component/ui/length.js
+++ app/js/component/ui/length.js
@@ -9,16 +9,43 @@ define(['flight/lib/component'], function(defineComponent) {
     function lengthComponent() {
 
         this.attributes({
-            permanentSelector: '#permanent'
+            permanentSelector: '#permanent',
+            monthsPartSelector: '#months',
+            monthsSelector: '#months > input'
         });
 
         this.permanentCheckboxWasClicked = function() {
-            alert('checkbox clicked');
+            if (this.select('permanentSelector').is(':checked')) {
+                this.$node.attr('data-length', 'Permanent');
+            } else {
+                var months = this.select('monthsSelector').val() + ' months';
+                this.$node.attr('data-length', months);
+            }
+
+            this.select('monthsPartSelector').toggleClass('invisible');
+        };
+
+        this.onlyNumbers = function(event) {
+            var currentValue = event.target.value;
+            event.target.value = currentValue.replace(/[^0-9\.]/g, '');
         };
 
         this.after('initialize', function() {
+            var lengthValue = this.$node.attr('data-length');
+            if (lengthValue === 'Permanent') {
+                this.select('permanentSelector').prop('checked', true);
+                this.select('monthsPartSelector').addClass('invisible');
+            } else {
+                this.select('permanentSelector').prop('checked', false);
+                this.select('monthsPartSelector').removeClass('invisible');
+            }
+
             this.on('click', {
                 permanentSelector: this.permanentCheckboxWasClicked
+            });
+
+            this.on('keyup', {
+                monthsSelector: this.onlyNumbers
             });
         });
     }
